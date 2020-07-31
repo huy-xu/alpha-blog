133e0c255a60729637030c1f9d08642a68e8ae51
huy-xu <nhathuyhedspi@gmail.com>
2020-07-31 17:05:13 +0700

    Create edit template and action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,6 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params .require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description)
   end
 end
